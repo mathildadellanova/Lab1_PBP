761af43bdf979c959056294b752c6075915038e6
mathildadellanova <mathilda.dellanova@ui.ac.id>
2022-09-22 12:11:22 +0700

    Menyelesaikan Tutorial 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from wishlist.views import show_wishlist
-from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
-from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
-from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_xml 
+from wishlist.views import show_json 
+from wishlist.views import show_json_by_id 
+from wishlist.views import register
+from wishlist.views import login_user 
+from wishlist.views import logout_user 
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
-    path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
-    path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('xml/', show_xml, name='show_xml'), 
+    path('json/', show_json, name='show_json'), 
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
